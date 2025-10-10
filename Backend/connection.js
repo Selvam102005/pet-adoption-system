22a8d1d63942a375c1adebdb80a1c2e166087993
Selvam102005 <selvam.s102005@gmail.com>
2025-10-10 21:47:47 +0530

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb+srv://selvam:Mallika%40102005@cluster-1.nvjoxzk.mongodb.net/";
+const MONGO_URL = "your_mongodb_url";
 const connectDB = async () => {
   try {
     const con = await mongoose.connect(MONGO_URL);
